c683a4a3923906b96bd083f1bfb430d99ea28cb5
Chu A. <chu.amk22@gmail.com>
2025-11-28 11:34:23 +0100

    feat(ui): Implement the SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,24 @@
+const SearchBar = () => {
+  return (
+    <form
+      onClick={(e) => e.preventDefault()}
+      className=" bg-[#1d2937] border border-gray-500 mx-auto mt-8 relative  min-w-sm max-w-2xl flex flex-col md:flex-row items-center justify-center py-2 px-2 rounded-xl gap-2 shadow-2xl focus-within:border-gray-300"
+    >
+      <input
+        className="px-6 py-2 w-full rounded-md flex-1 outline-none text-gray-500"
+        type="text"
+        placeholder="Enter GitHub username (e.g.,torvalds)"
+        required
+      />
+      <button className="w-full md:w-auto px-6 py-3 bg-[#145dfb] text-white fill-white active:scale-95 duration-100 will-change-transform overflow-hidden relative rounded-xl transition-all cursor-pointer hover:bg-[#2a6bf7]">
+        <div className="flex items-center transition-all ">
+          <span className="text-sm font-semibold whitespace-nowrap truncate mx-auto ">
+            Search
+          </span>
+        </div>
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
