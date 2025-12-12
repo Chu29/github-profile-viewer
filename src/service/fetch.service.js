540eed540b10dc5fdf9f050836bb7a1c4f7c7eec
Chu A <chu.amk22@gmail.com>
2025-12-12 08:48:26 +0100

    refactor: create handlers to handle fetch UserInfo and UserRepos

--- src/service/fetch.service.js
+++ src/service/fetch.service.js
@@ -1,6 +1,11 @@
 import { api } from "./api";
 
-export const getUserInfo = async (name) => {
-  const res = api.get(`users/${name}`);
+export const getUserInfo = async (query) => {
+  const res = api.get(`users/${query}`);
+  return res.json();
+};
+
+export const getUserRepos = async (query) => {
+  const res = api.get(`users/${query}/repos?per_page=10&sort=pushed`);
   return res.json();
 };
