fd7b9292e60451d332cd054fb8640bba870ab710
Chu A <chu.amk22@gmail.com>
2025-12-12 08:48:58 +0100

    chore: configure route for StatsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import StatsPage from "./pages/StatsPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/users/:query" Component={StatsPage} />
       </Routes>
     </BrowserRouter>
   );
